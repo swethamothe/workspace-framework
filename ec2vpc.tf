8d2a62c6238f6570153bdb4bf300c208c60faaec
swethamothe <89130824+swethamothe@users.noreply.github.com>
2022-04-05 16:01:33 +0100

    Create ec2vpc.tf

---
+++ ec2vpc.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "main" {
+	ami           = var.ami
+	instance_type = var.instance_type
+    subnet_id = aws_subnet.subnet.id
+	tags = {
+		Name = var.project_name
+	}
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.vpc
+}
+
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = var.subnet
+	availability_zone = "eu-west-2a"
+}

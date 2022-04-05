c46dd4be0388a8b4d5fba828932f1b16401e380f
swethamothe <89130824+swethamothe@users.noreply.github.com>
2022-04-05 12:24:23 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+	region  = local.workspace["vpc_region"]
+}
+
+module "ec2vpc" {
+    source = ".//modules"
+
+      vpc_region  = local.workspace["vpc_region"]
+      env_name = local.workspace["env_name"]
+      project_name  = local.workspace["project_name"]
+      ami = local.workspace["ami"]
+      vpc = local.workspace["vpc"]
+      subnet = local.workspace["subnet"]
+      instance_type = local.workspace["instance_type"]
+}
